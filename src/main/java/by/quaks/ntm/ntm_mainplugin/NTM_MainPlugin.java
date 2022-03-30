e10d57f9cedfbfb0e885f341592d2856c7adf5ee
Quaks <egorlapatskij@gmail.com>
2022-03-30 20:36:46 +0300

    Добавлен класс для тестирования ивентов

--- src/main/java/by/quaks/ntm/ntm_mainplugin/NTM_MainPlugin.java
+++ src/main/java/by/quaks/ntm/ntm_mainplugin/NTM_MainPlugin.java
@@ -1,5 +1,6 @@
 package by.quaks.ntm.ntm_mainplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NTM_MainPlugin extends JavaPlugin {
@@ -8,6 +9,7 @@ public final class NTM_MainPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("NTM - Основной плагин запущен");
+        Bukkit.getPluginManager().registerEvents(new TestEvents(), this);
     }
 
     @Override
---
+++ src/main/java/by/quaks/ntm/ntm_mainplugin/TestEvents.java
@@ -0,0 +1,6 @@
+package by.quaks.ntm.ntm_mainplugin;
+
+import org.bukkit.event.Listener;
+
+public class TestEvents implements Listener {
+}
