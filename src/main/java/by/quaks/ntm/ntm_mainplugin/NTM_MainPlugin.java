a41b278e31a0ca6a3df0afaab90514b07755a3fb
Quaks <egorlapatskij@gmail.com>
2022-04-16 00:01:47 +0300

    Подключил модули DiscordSRV

--- src/main/java/by/quaks/ntm/ntm_mainplugin/NTM_MainPlugin.java
+++ src/main/java/by/quaks/ntm/ntm_mainplugin/NTM_MainPlugin.java
@@ -6,14 +6,27 @@ import by.quaks.ntm.ntm_mainplugin.commands.TellCommand;
 import by.quaks.ntm.ntm_mainplugin.commands.BurpCommand;
 import by.quaks.ntm.ntm_mainplugin.listeners.ChatEventListener;
 import by.quaks.ntm.ntm_mainplugin.listeners.ClickOnPlayerEventListener;
+import by.quaks.ntm.ntm_mainplugin.listeners.DiscordSRVListener;
 import by.quaks.ntm.ntm_mainplugin.listeners.MuteListener;
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import github.scarsz.discordsrv.util.DiscordUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NTM_MainPlugin extends JavaPlugin {
+public final class NTM_MainPlugin extends JavaPlugin implements Listener {
+
+    private DiscordSRVListener discordsrvListener = new DiscordSRVListener(this);
 
     @Override
     public void onEnable() {
+        DiscordSRV.api.subscribe(discordsrvListener);
+        getServer().getPluginManager().registerEvents(this, this);
         // Plugin startup logic
         //System.out.println("NTM - Основной плагин запущен");
         getLogger().info("NTM - Основной плагин запущен");
@@ -27,10 +40,30 @@ public final class NTM_MainPlugin extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new MuteListener(),this);
     }
 
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(event.getPlayer().getUniqueId());
+        if (discordId == null) {
+            event.getPlayer().sendMessage(ChatColor.RED + "You are not linked");
+            return;
+        }
+
+        User user = DiscordUtil.getJda().getUserById(discordId);
+        if (user == null) {
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "Couldn't find the user you're linked to");
+            return;
+        }
+
+        event.getPlayer().sendMessage(ChatColor.GREEN + "You're linked to " + user.getAsTag());
+    }
+
+
+
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         //System.out.println("NTM - Основной плагин выключен");
         getLogger().info("NTM - Основной плагин выключен");
+        DiscordSRV.api.unsubscribe(discordsrvListener);
     }
 }
