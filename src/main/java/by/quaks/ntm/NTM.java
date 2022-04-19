47468578257a6caae3bc069aee20a6c7d274baaa
Quaks <egorlapatskij@gmail.com>
2022-04-19 04:01:51 +0300

    Конфиг файл

--- src/main/java/by/quaks/ntm/NTM.java
+++ src/main/java/by/quaks/ntm/NTM.java
@@ -1,26 +1,29 @@
 package by.quaks.ntm;
 
-import by.quaks.ntm.commands.BurpCommand;
-import by.quaks.ntm.commands.IgnoreCommand;
-import by.quaks.ntm.commands.PMuteCommand;
-import by.quaks.ntm.commands.TellCommand;
+import by.quaks.ntm.commands.*;
 import by.quaks.ntm.listeners.*;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class NTM extends JavaPlugin implements Listener {
 
     private DiscordSRVListener discordsrvListener = new DiscordSRVListener(this);
     private DeathListenerSRV deathListener = new DeathListenerSRV(this);
-
+    static NTM main;
+    public static NTM getInstance(){
+        return main;
+    }
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerJoin(PlayerJoinEvent event) {
         String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(event.getPlayer().getUniqueId());
@@ -40,27 +43,48 @@ public final class NTM extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        main = this;
         DiscordSRV.api.subscribe(discordsrvListener);
         DiscordSRV.api.subscribe(deathListener);
-        getServer().getPluginManager().registerEvents(this, this);
-        // Plugin startup logic
-        //System.out.println("NTM - Основной плагин запущен");
         getLogger().info("NTM - Основной плагин запущен");
         getCommand("msg").setExecutor(new TellCommand());
         getCommand("msg").setTabCompleter(new TellCommand());
         getCommand("burp").setExecutor(new BurpCommand());
         getCommand("ignore").setExecutor(new IgnoreCommand());
         getCommand("permmute").setExecutor(new PMuteCommand());
+        getCommand("reply").setExecutor(new ReplyCommand());
         Bukkit.getPluginManager().registerEvents(new ChatEventListener(), this);
         Bukkit.getPluginManager().registerEvents(new ClickOnPlayerEventListener(), this);
         Bukkit.getPluginManager().registerEvents(new MuteListener(),this);
         Bukkit.getPluginManager().registerEvents(new DeathListener(),this);
+        getServer().getPluginManager().registerEvents(this, this);
+        File file = new File(getDataFolder() + File.separator + "config.yml");
+        if (!file.exists()){
+            getConfig().addDefault("channels.admin", "000000000000000000");
+            getConfig().addDefault("channels.report", "000000000000000000");
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        } else {
+            CheckConfig();
+            saveConfig();
+            reloadConfig();
+        }
     }
-
+    public void CheckConfig() {
+        if(getConfig().get("channels.admin") == null){
+            getConfig().set("channels.admin", "000000000000000000");
+            saveConfig();
+            reloadConfig();
+        }
+        if(getConfig().get("channels.report") == null){
+            getConfig().set("channels.report", "000000000000000000");
+            saveConfig();
+            reloadConfig();
+            getConfig();
+        }
+    }
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        //System.out.println("NTM - Основной плагин выключен");
         getLogger().info("NTM - Основной плагин выключен");
         DiscordSRV.api.unsubscribe(discordsrvListener);
         DiscordSRV.api.unsubscribe(deathListener);
